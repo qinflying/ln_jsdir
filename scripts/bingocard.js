775874f70234b58eec8ecbf9dd33501387c6b2f7
ubuntu_ws <qinflying@outlook.com>
2020-05-31 00:24:46 +0800

    bingo card game reload

--- scripts/bingocard.js
+++ scripts/bingocard.js
@@ -1,8 +1,42 @@
 window.onload = initBingoCard;
 
+var colPlace = new Array(0,0,0,0,0,1,1,1,1,1,2,2,2,2,2,3,3,3,3,3,4,4,4,4,4);
+var useNums = new Array(76);
 function initBingoCard(){
-    for(var i=0; i < 24; i++){
-        var newNum = Math.floor(Math.random() * 75)+1;
-        document.getElementById("cell"+i).innerHTML = newNum;
+    document.getElementById("reload").onclick = ReloadCard;
+    NewCard();
+}
+
+function NewCard(){
+    for(var i = 0; i < 24; i++){
+        SetNewNum(i);
     }
+}
+
+function ReloadCard(){
+    ClearBook();
+    NewCard();
+    return false;
+}
+
+function ClearBook(){
+    for(var i = 0; i < useNums.length; i++){
+        useNums[i]=false;
+    }
+}
+
+function SetNewNum(i){
+    var colBasis = colPlace[i] * 15;
+    var newNum;
+    do{
+       newNum =  colBasis + Math.floor(Math.random() * 15) + 1;
+    }while(useNums[newNum]);
+    
+    useNums[newNum] = true;
+    document.getElementById("cell"+i).innerHTML = newNum;
+   
+}
+
+function randomNewNum(){
+    return Math.floor(Math.random() * 15) + 1;
 }

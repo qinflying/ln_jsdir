80dc4dc5853a0f59a5f6cc95f57369e322695052
ubuntu_ws <qinflying@outlook.com>
2020-06-01 08:23:51 +0800

    bingo winner

--- scripts/bingocard2.js
+++ scripts/bingocard2.js
@@ -60,4 +60,35 @@ function toggleColor(evt){
     else{
         oCell.className = "";
     }
+    checkWin();
+}
+
+function checkWin(){
+    var winningOption = -1;
+    var setCells = 0;
+    var winners = new Array(31, 992, 15360, 507904,541729,557328,1083458,2162820,4329736,8519745,8659472,16252928);
+    
+    for(var i = 0; i < 24; i++){
+        var sCell = "cell"+i;
+        var oCell = document.getElementById(sCell);
+        if(oCell.className != ""){
+            oCell.className = "pickedBG";
+            setCells = setCells | Math.pow(2, i);
+        }
+    }
+
+    for(var i = 0; i < winners.length; i++){
+        if((winners[i] & setCells) == winners[i]){
+            winningOption = i;
+        }
+    }
+
+    if(winningOption > -1){
+        //alert("to do winning!");
+        for(var i = 0; i < 24; i++){
+            if(winners[winningOption] & Math.pow(2, i)){
+                document.getElementById("cell"+i).className = "winningBG";
+            }
+        }
+    }
 }
